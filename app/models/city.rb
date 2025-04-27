f1127aaa6d13229ca8505acdb4f3591882756cac
achrafbsibiss <achraf.bsibiss11@gmail.com>
2025-04-27 20:56:59 +0100

    Add the table we need

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,6 @@
 class City < ApplicationRecord
+  has_many :origin_tolls, class_name: "Toll", foreign_key: "origin_id"
+  has_many :destination_tolls, class_name: "Toll", foreign_key: "destination_id"
+
+  validates :name, presence: true, uniqueness: true
 end
